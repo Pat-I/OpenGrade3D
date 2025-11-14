b85e8bec6f8132f39dfd9af1c1ce3a65743c0a38
Pat-I <66231833+Pat-I@users.noreply.github.com>
2025-11-13 21:10:19 -0500

    Framework4.8

--- SourceCode/GPS/Properties/Vehicle.Designer.cs
+++ SourceCode/GPS/Properties/Vehicle.Designer.cs
@@ -12,7 +12,7 @@ namespace OpenGrade.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Vehicle : global::System.Configuration.ApplicationSettingsBase {
         
         private static Vehicle defaultInstance = ((Vehicle)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Vehicle())));
